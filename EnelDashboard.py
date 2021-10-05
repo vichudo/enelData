b5f171438054ab90a8c6765cbd6024ca6d3681f8
vichudo <vichudo@gmail.com>
2021-10-05 15:28:55 -0300

    Update EnelDashboard.py

--- EnelDashboard.py
+++ EnelDashboard.py
@@ -5,23 +5,51 @@ import matplotlib as plt
 import seaborn as sns
 
 #plt.figure(figsize=(21,10))
-st.title("Análisis 4A y Distribción en el tiempo")
+st.title("Análisis 4A y Distribución en el tiempo")
 #habilidad = "Agilizar"
 habilidad = st.selectbox(
 'Elige la habilidad',
 ('Agilizar', 'Asesorar', 'Acoger', 'Asistir'))
 
+proveedor = st.selectbox(
+'Elige el proveedor',
+('UPCOM', 'KONECTA', 'AMBOS JUNTOS','AMBOS SEPARADOS'))
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
-sns.kdeplot(
-    data=getData(df_total.reset_index(),"UPCOM",habilidad), x="SEMANA", y="SCORE",
-    fill=True, thresh=0, levels=100, cmap="mako")
-st.pyplot()
+if proveedor == "UPCOM":
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    sns.kdeplot(
+        data=getData(df_total.reset_index(),"UPCOM",habilidad), x="SEMANA", y="SCORE",
+        fill=True, thresh=0, levels=100, cmap="mako")
+    st.pyplot()
+
+if proveedor == "KONECTA":
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    #plt.figure(figsize=(21,10))
+    fig2 = sns.kdeplot(
+        data=getData(df_total.reset_index(),"KONECTA",habilidad), x="SEMANA", y="SCORE",
+        fill=True, thresh=0, levels=100, cmap="rocket")
+    st.pyplot()
+
+if proveedor == "AMBOS SEPARADOS":
+    
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    sns.kdeplot(
+        data=getData(df_total.reset_index(),"UPCOM",habilidad), x="SEMANA", y="SCORE",
+        fill=True, thresh=0, levels=100, cmap="mako")
+    st.pyplot()
+
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    sns.kdeplot(
+        data=getData(df_total.reset_index(),"KONECTA",habilidad), x="SEMANA", y="SCORE",
+        fill=True, thresh=0, levels=100, cmap="rocket")
+    st.pyplot()
 
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
-#plt.figure(figsize=(21,10))
-fig2 = sns.kdeplot(
-    data=getData(df_total.reset_index(),"KONECTA",habilidad), x="SEMANA", y="SCORE",
-    fill=True, thresh=0, levels=100, cmap="rocket")
-st.pyplot()
+if proveedor == "AMBOS JUNTOS":
+    
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    sns.kdeplot(
+        data=getData(df_total.reset_index(),"ALL",habilidad), x="SEMANA", y="SCORE",
+        fill=True, thresh=0, levels=100, cmap="mako")
+    st.pyplot()
+
